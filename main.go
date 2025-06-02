9090f15e181d01c29670175a7908f5cf3acbe8fe
AsherAnd <asher@AsherAnd-laptop.localdomain>
2025-06-02 00:14:35 -0400

    works until 1 million

--- main.go
+++ main.go
@@ -3,28 +3,37 @@ package main
 import "fmt"
 
 func num2geez(n int) string {
-	numberMap := map[int]rune{1: '፩', 2: '፪', 3: '፫', 4: '፬', 5: '፭', 6: '፮', 7: '፯', 8: '፰', 9: '፱',
-		10: '፲', 20: '፳', 30: '፴', 40: '፵', 50: '፶', 60: '፷', 70: '፸', 80: '፹', 90: '፺',
-		100: '፻', 10000: '፼'}
+	numberMap := map[int]rune{
+		1: '፩', 2: '፪', 3: '፫', 4: '፬', 5: '፭',
+		6: '፮', 7: '፯', 8: '፰', 9: '፱', 10: '፲',
+		20: '፳', 30: '፴', 40: '፵', 50: '፶', 60: '፷',
+		70: '፸', 80: '፹', 90: '፺', 100: '፻', 10000: '፼',
+	}
 
 	res := ""
-	place := 1
+	subscript := 0
 
 	for n > 0 {
-		value := n % 10
+		value := n % 100
 
-		if value != 0 {
-			if ((place%100 == 0) || place%10000 == 0) && value > 1 {
-				res = string(numberMap[place]) + res
-				place = 1
+		ones := value % 10
+		tens := value / 10 * 10
+
+		if subscript > 0 && !(tens == 0 && ones == 0) {
+			if subscript%2 == 0 {
+				res = string(numberMap[10000]) + res
+			} else {
+				res = string(numberMap[100]) + res
 			}
-
-			res = string(numberMap[value*place]) + res
-
+			if tens == 0 && ones == 1 {
+				ones = 0
+			}
 		}
 
-		n /= 10
-		place *= 10
+		res = string(numberMap[tens]) + string(numberMap[ones]) + res
+
+		n /= 100
+		subscript++
 
 	}
 
@@ -32,10 +41,15 @@ func num2geez(n int) string {
 }
 
 func main() {
-	for i := 1; i <= 1000; i++ {
-		fmt.Println(i, num2geez(i))
-	}
-	// for i := 1; i <= 1000000; i *= 10 {
+	// for i := 1; i <= 100; i++ {
 	// 	fmt.Println(i, num2geez(i))
 	// }
+	for i := 1; i <= 1000000; i *= 10 {
+		fmt.Println(i, num2geez(i))
+	}
+
+	// fmt.Println(num2geez(170))
+	// fmt.Println(num2geez(200))
+	// fmt.Println(num2geez(1170))
+	// fmt.Println(num2geez(9999))
 }
